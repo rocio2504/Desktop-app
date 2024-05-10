6201a2ac8c6f379a9263aaba914b7d1b76c56c3d
rocio2504 <rocioandrea250489@gmail.com>
2024-05-10 09:05:06 -0500

    update function2

---
+++ ContenedoresApp/FormNuevo.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ContenedoresApp
+{
+    partial class FormNuevo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            erMessage = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)erMessage).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(221, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 20);
+            label1.TabIndex = 0;
+            label1.Text = "NÚMERO :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(221, 153);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 20);
+            label2.TabIndex = 1;
+            label2.Text = "TIPO :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(221, 209);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 20);
+            label3.TabIndex = 2;
+            label3.Text = "TAMAÑO :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(221, 259);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 20);
+            label4.TabIndex = 3;
+            label4.Text = "PESO : ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(221, 316);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 20);
+            label5.TabIndex = 4;
+            label5.Text = "TARA : ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(343, 96);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(151, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(343, 252);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(151, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(343, 309);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(151, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(343, 153);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 8;
+            comboBox1.Text = "Seleccione:";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(343, 209);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(607, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 10;
+            button1.Text = "GUARDAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(607, 174);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 11;
+            button2.Text = "LIMPIAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(607, 239);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 12;
+            button3.Text = "<-- ATRÁS";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // erMessage
+            // 
+            erMessage.ContainerControl = this;
+            // 
+            // FormNuevo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormNuevo";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormNuevo";
+            Load += FormNuevo_Load;
+            ((System.ComponentModel.ISupportInitialize)erMessage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ErrorProvider erMessage;
+    }
+}
---
+++ ContenedoresApp/FormNuevo.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ContenedoresApp
+{
+    public partial class FormNuevo : Form
+    {
+        ConexionBD sqlControl = new ConexionBD();
+
+        private int? Id;
+
+
+        public FormNuevo(int? Id = null)
+        {
+            InitializeComponent();
+            this.Id = Id;
+            if (this.Id != null)
+            {
+
+                cargarDatos();
+            }
+        }
+
+        private void cargarDatos()
+        {
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.loadData(Id);
+            //comboBox1.DataSource = tabla;
+            textBox1.Text = tabla.Rows[0][0].ToString();
+            comboBox1.Text = tabla.Rows[0][1].ToString();
+            comboBox2.Text = tabla.Rows[0][2].ToString();
+            textBox2.Text = tabla.Rows[0][3].ToString();
+            textBox3.Text = tabla.Rows[0][4].ToString();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //GUARDAR
+            // borrarMensajeError();
+
+            if (Id == null)
+            {
+                if (verificarCampos())
+                {
+                    try
+                    {
+
+
+                        string r = sqlControl.GuardarContenedor(int.Parse(textBox1.Text), comboBox1.Text, int.Parse(comboBox2.Text), float.Parse(textBox2.Text), float.Parse(textBox3.Text));
+
+                        MessageBox.Show("Se guardó correctamente");
+
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show("Ocurrió un error al guardar: " + exc.Message);
+                    }
+                    //  MessageBox.Show(r);
+                }
+
+                this.Close();
+
+            }
+            else
+            {
+                if (verificarCampos())
+                {
+                    try
+                    {
+
+
+                        string r = sqlControl.editarContenedor(int.Parse(textBox1.Text), comboBox1.Text, int.Parse(comboBox2.Text), float.Parse(textBox2.Text), float.Parse(textBox3.Text));
+
+                        MessageBox.Show("Se actualizó correctamente");
+
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show("Ocurrió un error al actualizar: " + exc.Message);
+                    }
+                    //  MessageBox.Show(r);
+                }
+
+                this.Close();
+
+
+
+            }
+
+
+        }
+
+        private bool verificarCampos()
+        {
+            bool ok = true;
+            if (textBox1.Text == "")
+            {
+                ok = false;
+                erMessage.SetError(textBox1, "Ingresar Número");
+            }
+            if (comboBox1.Text == "")
+            {
+                ok = false;
+                erMessage.SetError(comboBox1, "Ingresar Tipo");
+            }
+            if (comboBox2.Text == "")
+            {
+                ok = false;
+                erMessage.SetError(comboBox2, "Ingresar Tamaño");
+            }
+
+            if (textBox2.Text == "")
+            {
+                ok = false;
+                erMessage.SetError(textBox2, "Ingresar Peso");
+            }
+
+            if (textBox3.Text == "")
+            {
+                ok = false;
+                erMessage.SetError(textBox3, "Ingresar Tara");
+            }
+
+
+            return ok;
+
+        }
+        private void FormNuevo_Load(object sender, EventArgs e)
+        {
+            //cargar combo tipo
+            cargarComboTipo();
+            //cargar combo tamaño
+            cargarComboTamanio();
+        }
+
+        private void cargarComboTipo()
+        {
+
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.cargarComboTipo();
+            //tabla.Add
+            comboBox1.DataSource = tabla;
+            comboBox1.DisplayMember = "Descripcion";
+            comboBox1.ValueMember = "IdTipo";
+        }
+        private void cargarComboTamanio()
+        {
+
+            DataTable tabla = new DataTable();
+            tabla = sqlControl.cargarComboTamanio();
+            comboBox2.DataSource = tabla;
+            comboBox2.DisplayMember = "Descripcion";
+            comboBox2.ValueMember = "IdTamanio";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
